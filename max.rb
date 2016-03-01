d68e4d952ec5143662c8b6c7618de1db1319b409
AjayMahen <ajanmahen@gmail.com>
2016-03-01 19:05:09 +0000

    Maximum Value with 2 methods

--- max.rb
+++ max.rb
@@ -1,17 +1,39 @@
 # Find the maximum 
 def maximum(arr)
-  arr.max
+  	x = arr.length
+  	if x > 0
+		until x == 1 
+	  		if arr[x-1] > arr[x-2]
+	  			arr.delete_at(x-2)
+	  		else
+	  			arr.delete_at(x-1)
+	  		end
+	  		x = arr.length
+	  	end
+	  	return arr  
+  	else
+  		return nil
+  	end
+end
+
+
+def maximum_b(arr)
+	highest = arr.first
+	arr.each do |x|
+		highest = x > highest ? x : highest
+	end
+	highest
 end
 
 # expect it to return 42 below
-result = maximum([2, 42, 22, 02])
+result = maximum_b([2, 42, 22, 02])
 puts "max of 2, 42, 22, 02 is: #{result}"
 
 # expect it to return nil when empty array is passed in
 result = maximum([])
 puts "max on empty set is: #{result.inspect}"
 
-result = maximum([-23, 0, -3])
+result = maximum_b([-23, 0, -3])
 puts "max of -23, 0, -3 is: #{result}"
 
 result = maximum([6])
